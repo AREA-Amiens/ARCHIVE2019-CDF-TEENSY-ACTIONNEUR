c1187a57c14692127965b8be952bbb9177e26b51
albanlefe <albanlefebvre1775@gmail.com>
2019-03-19 10:17:09 +0100

    19/03/2018

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 //initialisation du moteur gauche
 
-byte a=0,etat=0;
-int b=0;
+byte a=0,etat=0,com=0,fair;
+long time2=0;
 
 AccelStepper motor_G(1, step_G, dir_G);//declaration du moteur gauche
 AccelStepper motor_D(1, step_D, dir_D);//declatation du moteur droit
@@ -12,12 +12,13 @@ AccelStepper motor_D(1, step_D, dir_D);//declatation du moteur droit
 Servo servo_d;
 Servo servo_g;
 
-void IntrerrupTimer (){
-  b++;
-}
 
 
 void setup() {
+
+  Wire.begin(my_adr);
+  Wire.onReceive(receiveEvent);//donne la fonction a ouvrire l'or de la réception de dune trasmition
+  Wire.onRequest(requestEvent);
   // put your setup code here, to run once:
   pinMode(reset_G, OUTPUT);    //le reset se fait à l'état bas
   digitalWrite(reset_G, HIGH);
@@ -36,17 +37,22 @@ void setup() {
   motor_D.setSpeed(speed);
   motor_D.setAcceleration(acceleration_rd);
 
-  pinMode(Led,OUTPUT);
 
-  MsTimer2::set(10,IntrerrupTimer);//tout les centiéme de seconde
-
-
+  MsTimer2::set(1000,IntrerrupTimer);//tout les  seconde
+  MsTimer2::start();
 
   servo_d.attach(22);
-//  servo_d.write(0);
-//  delay(3000);
   servo_g.attach(23);
-//  servo_g.write((int)((180-10)*0.755555555));
+//-----------------------------------------------------------------------------------------------------//
+  servo_d.write((int)50*0.7555555);//changer pour la mise a zero
+  //servo_g.write(0);
+  time2=0;
+  while(time2!=2){
+    Serial.println(time2);
+  }
+  servo_g.write((int)(180-90)*0.75555555555);
+//-------------------------------------------------------------------------------------------------------//
+
 //  delay(3000);
 //  servo_g.write((int)((180-90)*0.755555555));//180°à0° ecrire 136à6
 //  delay(95);
@@ -62,19 +68,38 @@ void loop() {
     else digitalWrite(Led,LOW);*/
   switch(etat){
     case 1:    //attante de trame
+      if(com==1){
+        etat=fair;
+        time2=0;
+      }
     break;
     case 2://ouveture eguille
-      servo_d.Write(0);
-      MsTimer2::start();
+    //--------------------------------------------------------------------------------------------------//
+      //angle d'eguille ouver
+        servo_g.write((int)((180-90)*0.755555555));
+      if(time2==1){
+        servo_d.write((int)((90)*0.75555555));
+        etat=1;
+        com=0;
+      }
+    //---------------------------------------------------------------------------------------------------//
     break;
+
+
     case 3://fermeture eguille
+      servo_d.write(0);
+      if(time2==1){
+        servo_g.write(90*0.755555555);
+        etat=1;
+        com=0;
+      }
     break;
     case 4:
     break;
     case 5:
     break;
     case 6:
-    break.
+    break;
     case 7:
     break;
     case 8:
@@ -89,4 +114,16 @@ void loop() {
     break;
 
   }
+}
+void receiveEvent(int howMany){//fonction d'intérupetion l'or dun envoi du maitre
+  byte i;//variable pour le for
+  fair=Wire.read();//rampli le tableau si avec les valeur de la transmition
+  com=1;// passe la comme a 1 pour l'éxecution de la trame en cour
+}
+void requestEvent(){//fonciton d'intérupetion l'or d une deamande de trame
+  Wire.write(com);//le maitre lira la valeur de com
+}
+void IntrerrupTimer (){
+  time2++;
+  //Serial.println("reuasie2");
 }
